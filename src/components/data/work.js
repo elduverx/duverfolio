3da6deaa1d17821082babc3ed5273c87709d48bd
elduverx <duverwild@gmail.com>
2023-04-04 01:53:34 +0200

    modified images.

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -4,7 +4,7 @@ export const WorkData=[
             designation: "Software Engineer",
             dateJoining: ("01-02-2021"),
             dateEnd: "Present",
-            companyLogo: "https://openailabsprodscus.blob.core.windows.net/private/user-iIABXOimvxE7KBE2DcibLpdS/generations/generation-AOEGu1s38J4DGeCPorVzNjnz/image.webp?st=2023-04-03T14%3A37%3A13Z&se=2023-04-03T16%3A35%3A13Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-03T14%3A12%3A39Z&ske=2023-04-10T14%3A12%3A39Z&sks=b&skv=2021-08-06&sig=ySLP1/TzU%2BszBo7RfVEbL1CzaqxaV8w1TALeaCVg7nU%3D",
+            companyLogo: "https://i.imgur.com/wttxdqX.png",
             work: "I am proficient in using iOS frameworks and libraries such as UIKit, Core Data, Core Animation, and more. I have strong debugging and troubleshooting skills to ensure that the applications work optimally on various iOS devices and versions.                       On my github, i leave more public projects, you can check also!"
         },
         {
